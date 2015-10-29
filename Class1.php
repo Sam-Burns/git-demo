0d5e118bb8c9e87c9c7e8734cdade3cc07ef135e
Sam Burns <sam.burns.uk@gmail.com>
2015-10-29 13:05:02 +0000

    Initial commit

---
+++ Class1.php
@@ -0,0 +1,12 @@
+<?php
+
+class Class1
+{
+    /**
+     * @return string
+     */
+    public function getHelloWorld()
+    {
+        return 'THIS IS A BUG!!!!!!';
+    }
+}
---
+++ Class2.php
@@ -0,0 +1,18 @@
+<?php
+
+class Class2
+{
+    /**
+     * @return string[]
+     */
+    public function getListOfNames()
+    {
+        return [
+            'Alice',
+            'Bob',
+            'Charly',
+            'Dennis',
+            'Emma',
+        ];
+    }
+}
